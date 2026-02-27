3e1187e79e68e1f65d7a33b6dcd985e97f56d9c5
Jessie Anderson <housewifehacker@gmail.com>
2026-02-27 13:44:00 -0400

    Rename public to docs for github pages

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["POSTS_DIR"] = "content"
-app.config["OUTPUT_DIR"] = "public"
+app.config["OUTPUT_DIR"] = "docs"
 
 from app import routes
